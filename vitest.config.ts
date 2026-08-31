9206f5c314eee6211b5c5d758a44e640250d8358
Aizzul Luqman <106507866+itsLucas02@users.noreply.github.com>
2026-09-01 01:22:38 +0800

    test: configure project path alias

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { defineConfig } from "vitest/config";
+
+const rootDir = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": rootDir,
+    },
+  },
+});
